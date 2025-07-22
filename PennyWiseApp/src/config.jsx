ca4b051f0a73009bdfd2807f7236248b85bac9a0
Tan Woon Kin <woonkin14@gmail.com>
2025-07-22 09:30:39 +0800

    Changes to configuration

--- PennyWiseApp/src/config.jsx
+++ PennyWiseApp/src/config.jsx
@@ -1,14 +1,13 @@
 import Constants from'expo-constants';
 
 // storeClient is for deploying in expo go, check standAlone for APK
-/* isDeployment = Constants.executionEnvironment === "storeClient";
+ isDeployment = Constants.executionEnvironment === "storeClient";
 // alias for the host machine
 // for deployment, change back
 export const server_base_URL = isDeployment
     ? "https://backend-1-j18w.onrender.com"
     : "http://10.0.2.2:5000";
-*/
 
 /* for development */
-export const server_base_URL = "http://10.0.2.2:5000";
+//export const server_base_URL = "http://10.0.2.2:5000";
 console.log("Base URL being used:", server_base_URL);
